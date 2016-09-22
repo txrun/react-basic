6a6a631dcf1841d15bb0598d0f340cd30cc8bc0d
Tarun Mathew <crushd@gmail.com>
2016-09-22 19:03:05 +0530

    Single component with an input field.

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+import './App.css';
+
+var App = React.createClass({
+  
+  render: function(){
+    return(
+        <div>
+          <input type="text" />
+        </div>
+      )
+  }
+});
+
+export default App;
