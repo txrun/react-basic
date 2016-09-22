48ef10ce36c771df94f2c4bd71049a4ea9e0d885
Tarun Mathew <crushd@gmail.com>
2016-09-22 20:26:37 +0530

    Added handleNameChange
    Updated code to reflect input typed in the text field right below it.

--- src/App.js
+++ src/App.js
@@ -4,10 +4,21 @@ import './App.css';
 
 var App = React.createClass({
   
+  getInitialState: function() {
+    return {
+      name: "tiper"
+    }
+  },
+
+  handleNameChange: function(e) {
+    this.setState({name: e.target.value});
+  },
+
   render: function(){
     return(
         <div>
-          <input type="text" />
+          <input type="text" value={this.state.name} onChange={this.handleNameChange}/>
+          <h2>{this.state.name}</h2>
         </div>
       )
   }
